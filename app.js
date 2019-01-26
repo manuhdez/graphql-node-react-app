dd414dd0e70b5adc963584fdf98c4bbf592e750f
manuhdez <manu.hdez.dev@gmail.com>
2019-01-26 17:10:47 +0000

    #3 schemas & resolvers

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphQlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
@@ -7,8 +9,29 @@ const app = express();
 app.use(bodyParser.json());
 
 // App routing
-app.get('/', (req, res, next) => {
-    res.send('Hello world!');
-});
+app.use('/graphql', graphQlHttp({
+    schema: buildSchema(`
+        type rootQuery {
+            events: [String!]!
+        }
+
+        type rootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: rootQuery,
+            mutation: rootMutation
+        }
+    `),
+    rootValue: {
+        events: () => ['Cooking', 'Sailing', 'Coding'],
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql: true
+}));
 
 app.listen(3300);
