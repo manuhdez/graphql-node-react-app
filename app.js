702fbb2e7b59b47094ce366f6f448703cafbdaf9
Manu Hdez <manu.hdez.dev@gmail.com>
2019-05-12 12:50:57 +0100

    #13 - hitting the api

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 
 // custom middleware functions
 const isAuth = require('./middleware/is-auth');
+const allowCORS = require('./middleware/cors');
 
 // graphql
 const graphQlSchema = require('./graphql/schema/index');
@@ -14,6 +15,7 @@ const app = express();
 
 // Middleware
 app.use(bodyParser.json());
+app.use(allowCORS);
 app.use(isAuth);
 
 // App routing
@@ -25,6 +27,6 @@ app.use('/graphql', graphQlHttp({
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-cvt1i.mongodb.net/${process.env.DB_NAME}?retryWrites=true`,
   { useNewUrlParser: true })
-  .then(() => app.listen(3300, () => console.log(`server running on http://localhost:${3300}`)))
+  .then(() => app.listen(5000, () => console.log(`server running on http://localhost:${5000}/graphql`)))
   .catch(err => console.log(err));
 
